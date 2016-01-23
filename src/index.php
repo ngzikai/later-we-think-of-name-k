7c47d194d53c94ec064c41f6194c112ea3169f7d
Juliana Seng <hexcone@gmail.com>
2016-01-24 01:28:34 +0800

    Edit index.php UI

--- src/index.php
+++ src/index.php
@@ -1,17 +1,75 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Zi Kai
- * Date: 23 Jan 2016
- * Time: 17:52
- */
-?>
-
+<!DOCTYPE HTML>
+<!--
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
 <html>
-<head>
-    <title>Hello World!</title>
-</head>
-<body>
-<h1>HELLO WORLD!</h1>
-</body>
+	<head>
+		<title>LATERK by Later we think of name k.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<h1><a href="index.html">Laterk</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+			<section id="banner">
+				<i class="icon fa-qq"></i>
+				<h2>LATERK</h2>
+				<p>lol we help popz ppl sync time</p>
+				<input type="submit" class="special" value="ENTER TIME" />
+				<input type="reset" class="alt" value="CREATE EVENT" />
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<!--
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					-->
+					<ul class="copyright">
+						<li>&copy; Later we think of name k.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
--- src/main.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <title>INTERNATIONAL TIMEZONE EVENT PLANNER</title>
-</head>
-<body>
-    <h3>INTERNATIONAL TIMEZONE EVENT PLANNER</h3>
-    <button type="button">ENTER TIME</button>                      <button type="button">CREATE EVENT</button>
-</body>
-</html>
