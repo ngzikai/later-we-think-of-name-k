089b50908cfa14fa370e59094c14b1d8625ff7b2
ngzikai <ngzikai92@gmail.com>
2016-01-24 00:22:54 +0800

    loadDictionary

--- src/assets/txt/loadDictionary.php
+++ src/assets/txt/loadDictionary.php
@@ -1,6 +1,11 @@
 <?php
-
-while(!feof($file)){
-    $line = fgets($file);
+$file_handle = @fopen("http://laterk.stellarmen.com/assets/txt/dictionary.txt", "r");
+if ($file_handle) {
+    while (($line = fgets($file_handle, 4096)) !== false) {
+        echo $line."<br>";
+    }
+    if (!feof($file_handle)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($file_handle);
 }
-fclose($file);
