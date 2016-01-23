d98e2791b0757f754c21234e3aa3bde647721634
Juliana Seng <hexcone@gmail.com>
2016-01-24 02:12:06 +0800

    Edit submitEvent.hp UI

--- src/newEvent.php
+++ src/newEvent.php
@@ -1,13 +1,89 @@
+<!DOCTYPE HTML>
+<!--
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
 <html>
-<head>
-    <title>Laterk</title>
-</head>
-<body>
- <form action="submit_newEvent.php" method="post">
-  Event name:<br>
-  <input type="text" name="event_name"><br>
-   <input type="submit" value="Submit">
-   <?php echo 'insert' . $_GET['msg'] ;?>
-</form> 
-</body>
+	<head>
+		<title>LATERK by Later we think of name k.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<h1><a href="index.html">Laterk</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+				</ul>
+			</nav>
+
+		<!-- Four -->
+			<section id="four" class="wrapper style2 special">
+				<div class="inner">
+					<header class="major narrow">
+						<h2>Create event</h2>
+						<p>for ppl to join and stuff</p>
+					</header>
+					<form action="submit_newEvent.php" method="POST">
+						<div class="container 75%">
+							<div class="row uniform 50%">
+								<div class="12u$">
+									<input name="event_name" placeholder="Enter your event name" type="text" />
+									<?php echo 'insert' . $_GET['msg'] ;?>
+								</div>
+							</div>
+						</div>
+						<ul class="actions">
+							<li><input type="submit" class="special" value="Submit" /></li>
+						</ul>
+					</form>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<!--
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					-->
+					<ul class="copyright">
+						<li>&copy; Later we think of name k.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
