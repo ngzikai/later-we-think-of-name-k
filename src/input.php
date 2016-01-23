a2cb89d2c7b1f1288a4ca22fef3885bab286f28a
Juliana Seng <hexcone@gmail.com>
2016-01-24 02:38:23 +0800

    Edit input.php UI

--- src/input.php
+++ src/input.php
@@ -1,27 +1,100 @@
+<!DOCTYPE HTML>
+<!--
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
 <html>
-<head>
-    <title>InputTime</title>
-</head>
-<body>
-	Name:
-  	<input type="text" name="name">
-  	<br>
-  	<br>
-  	Preferred Time:
-  	<div id="time-range">
-    <p>Time Range: <span class="slider-time">10:00 AM</span> - <span class="slider-time2">12:00 PM</span>
-	</p>
-    	<div class="sliders_step1">
-        	<div id="slider-range"></div>
-    	</div>
-	</div>  
-  	<br>
-  	<br>
-  	<button type="button">Get Code Link</button> 
+	<head>
+		<title>LATERK by Later we think of name k.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
 
+		<!-- Header -->
+			<header id="header" class="alt">
+				<h1><a href="index.php">Laterk</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
 
-	<script src="assets/js/slider_time.js"></script>
-</body>
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="input.php">Enter time</a></li>
+					<li><a href="newEvent.php">Create event</a></li>
+				</ul>
+			</nav>
+
+		<!-- Four -->
+			<section id="four" class="wrapper style2 special">
+				<div class="inner">
+					<header class="major narrow">
+						<h2>Enter time</h2>
+						<p>when you are free to have fun!</p>
+					</header>
+					<form action="submit_newEvent.php" method="POST">
+						<div class="container 75%">
+							<div class="row uniform 50%">
+								<div class="12u$">
+									<input name="name" placeholder="Enter your name" type="text" />
+									<br /><br />
+									<p>Enter your preferred time</p>
+									<div id="time-range">
+										<p>Time Range: <span class="slider-time">10:00 AM</span> - <span class="slider-time2">12:00 PM</span></p>
+										<div class="sliders_step1">
+											<div id="slider-range"></div>
+										</div>
+									</div>  
+									<?php echo 'insert' . $_GET['msg'] ;?>
+								</div>
+							</div>
+						</div>
+						<ul class="actions">
+							<li><input type="submit" class="special" value="Get code link" /></li>
+						</ul>
+					</form>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<!--
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					-->
+					<ul class="copyright">
+						<li>&copy; Later we think of name k.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/slider_time.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
-
-
