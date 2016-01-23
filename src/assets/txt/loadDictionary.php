74aa3902b259fee3d8e356447bfa3b108e99372f
ngzikai <ngzikai92@gmail.com>
2016-01-24 00:45:30 +0800

    loadDictionary

--- src/assets/txt/loadDictionary.php
+++ src/assets/txt/loadDictionary.php
@@ -4,12 +4,19 @@ include 'connectDB.php';
 $file_handle = @fopen("http://laterk.stellarmen.com/assets/txt/dictionary.txt", "r");
 if ($file_handle) {
     while (($line = fgets($file_handle, 4096)) !== false) {
-        echo $line."<br>";
+        echo $line;
 
         $sql = "INSERT INTO dictionary (entry, used)
-                VALUES ($line, 0)";
+                VALUES (".$line.", 0)";
 
-        
+       /* $success = mysql_query($sql);
+
+        if($succcess){
+            echo $line. " was added successfully! :)";
+        }else{
+            echo $line . " was not added successfully :(.";
+        }*/
+
 
     }
     if (!feof($file_handle)) {
