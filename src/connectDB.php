7646e36280af38b8c819ddd77cfe5f4617dbdb67
William Tan <tanwcw@gmail.com>
2016-01-23 23:13:10 +0800

    edit connectdb.php

--- src/connectDB.php
+++ src/connectDB.php
@@ -1,7 +1,8 @@
 <?php
-$servername = "www.stellarmen.com";
-$username = "stellarm";
+$servername = "localhost";
+$username = "stellarm_laterk";
 $password = "h44cknr00ll";
+$dbname='stellarm_laterk';
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
