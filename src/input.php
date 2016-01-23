09b1c5742fa0883786f288b9c1aed31f9fe44d0e
William Tan <tanwcw@gmail.com>
2016-01-24 07:51:34 +0800

    no message

--- src/input.php
+++ src/input.php
@@ -38,8 +38,7 @@
 			function myFunction() {
 				var d = new Date();
 				var n = d.getTimezoneOffset();				
-				n=n/60;
-				document.getElementById("gmt").value = n;				
+				document.getElementById("gmt").value = n/60;				
 			}
 			window.onload = myFunction;
 			
