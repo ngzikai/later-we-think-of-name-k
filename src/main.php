4bcb28cbcf3098fae19b4c8852c940dd1a8ff2ef
Timothy Chong <timothychong93@gmail.com>
2016-01-23 22:39:47 +0800

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,5 +1,5 @@
 <?php
-    print ("I am the CHICKEN MAN");
+    print ("INTERNATIONAL TIMEZONE EVENT PLANNER");
     
     $enter_button = newt_button(5, 12, "Enter Time");
     $create_button = newt_button(10, 16, "Create Event");
