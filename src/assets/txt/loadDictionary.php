1d61f2316fcd71a2f23f4bb222445455f9e1b685
ngzikai <ngzikai92@gmail.com>
2016-01-24 01:15:12 +0800

    loadDictionary

--- src/assets/txt/loadDictionary.php
+++ src/assets/txt/loadDictionary.php
@@ -10,7 +10,7 @@ $dbname='stellarm_laterk';
 
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
