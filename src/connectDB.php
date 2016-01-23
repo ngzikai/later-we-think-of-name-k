5d9f77631953fb4e60622a57ade73971d0c4c795
William Tan <tanwcw@gmail.com>
2016-01-23 22:13:13 +0800

    connectdb.php update

--- src/connectDB.php
+++ src/connectDB.php
@@ -1,4 +1,17 @@
 <?php
+$servername = "www.stellarmen.com";
+$username = "stellarm_laterk";
+$password = "h4ccknr00ll";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?> 
 /**
  * Created by PhpStorm.
  * User: Zi Kai
