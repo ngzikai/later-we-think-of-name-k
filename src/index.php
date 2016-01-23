d45e8e0dc61201655328f96738512fcbd5aec122
ngzikai <ngzikai92@gmail.com>
2016-01-23 20:37:30 +0800

    Hello World

--- src/index.php
+++ src/index.php
@@ -5,3 +5,13 @@
  * Date: 23 Jan 2016
  * Time: 17:52
  */
+?>
+
+<html>
+<head>
+    <title>Hello World!</title>
+</head>
+<body>
+<h1>HELLO WORLD!</h1>
+</body>
+</html>
--- src/submit_newEvent.php
+++ src/submit_newEvent.php
@@ -5,3 +5,15 @@
  * Date: 23 Jan 2016
  * Time: 17:58
  */
+
+function generateRandomString($length = 8) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+echo generateRandomString();
