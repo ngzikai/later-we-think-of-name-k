138013b589f3def603266498a417c1a70b06c736
Timothy Chong <timothychong93@gmail.com>
2016-01-24 00:26:54 +0800

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,7 +1,9 @@
-<?php
-    print ("INTERNATIONAL TIMEZONE EVENT PLANNER");
-    
-    $enter_button = newt_button(5, 12, "Enter Time");
-    $create_button = newt_button(10, 16, "Create Event");
-?>
-
+<html>
+<head>
+    <title>INTERNATIONAL TIMEZONE EVENT PLANNER</title>
+</head>
+<body>
+    <h3>INTERNATIONAL TIMEZONE EVENT PLANNER</h3>
+    <button type="button">ENTER TIME</button>                      <button type="button">CREATE EVENT</button>
+</body>
+</html>
