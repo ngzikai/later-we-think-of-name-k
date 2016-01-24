247dee99fb099ca4980b03b41adb7eefc349522b
William Tan <tanwcw@gmail.com>
2016-01-24 08:28:01 +0800

    no message

--- src/input.php
+++ src/input.php
@@ -93,7 +93,7 @@
 								if ($_GET['event']==""){
 									echo '<li><input type="submit" class="special" value="Get code link" /></li>';
 								} else{
-									echo 'YOUR CODE IS '.$_GET['event'];
+									echo 'YOUR CODE IS <a href="eventMain.php?event="' . $_GET['event'] . ">".$_GET['event']."</a>" ;
 								}
 								?>
 						</ul>
