96e2398e6545e8cf4ddbc3dcfcab2162fb8327c9
William Tan <tanwcw@gmail.com>
2016-01-24 11:39:15 +0800

    no message

--- src/eventMain.php
+++ src/eventMain.php
@@ -91,7 +91,7 @@
 					    <form action="addNewParticipant.php" method="POST">
 						<h2>Add new participant</h2>
 						<input name="shortlink" placeholder="Enter an unique code link" type="text" />
-						<?php echo '<input name="event_code" type="hidden" value="'.$_GET['event'].'">'; ?>
+						<?php echo '<input type="hidden" name="event_code" value="'.$_GET['event'].'">'; ?>
 						<ul class="actions">
 							<li><input type="submit" class="special" value="Submit" /></li>
 						</ul>
